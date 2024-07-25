03573b7f9be304d15e08e1bd4033c8e8e7ee1a27
barrera77 <30675869+barrera77@users.noreply.github.com>
2024-07-24 23:57:32 -0600

    Complted at 70% Only

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  const esportsMenu = document.querySelector(".esports-menu");
+  const dropdownItems = document.querySelectorAll(".dropdownitem");
 
+  menuIcon.addEventListener("click", () => {
+    esportsMenu.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+  });
+
+  dropdownItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      dropdownItems.forEach((otherItem) => {
+        if (otherItem !== item) {
+          otherItem.classList.remove("active");
+        }
+      });
+
+      item.classList.toggle("active");
+    });
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !menuIcon.contains(event.target) &&
+      !esportsMenu.contains(event.target)
+    ) {
+      esportsMenu.classList.remove("active");
+      menuIcon.classList.remove("active");
+    }
+  });
+});
